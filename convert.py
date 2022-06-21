14e35940e4412f105cbc1eb1deb715f4d5c93c7c
Jeff Smith <toxicglados@gmail.com>
2022-06-20 19:52:27 -0500

    Use local database instead of scryfall api calls

--- convert.py
+++ convert.py
@@ -9,120 +9,109 @@
 
 import re, os, json, scrython, time
 
+# Use local scryfall database for this
 def get_default_collectors_number(name, set_abbr):
-    oracle_id = scrython.Named(exact=name).oracle_id()
-    page = 1
-    cards = scrython.Search(q=f'oracle_id:{oracle_id}', unique='prints', page=page)
-    total_cards = cards.total_cards()
-    data = cards.data()
-
-    while cards.has_more():
-        page += 1
-        time.sleep(.1)
-        cards = scrython.Search(q=f'oracle_id:{oracle_id}', unique='prints', page=page)
-        data.extend(cards.data())
-
-    assert len(data) == total_cards
-    in_correct_set = [card for card in data if card['set'].lower() == set_abbr.lower()]
-
-    default = min(in_correct_set, key=lambda card: card['collector_number'])['collector_number']
+    # Tappedout does Turn / Burn
+    # Scryfall  does Turn // Burn
+    if '/' in name and '//' not in name:
+        name = name.replace('/', '//')
+    cards = [card for card in scryfall_db if card['name'].lower() == name.lower() and card['set'].lower() == set_abbr.lower()]
+    # Cards with multiple faces, adventure cards, split cards, etc. will have a combined name
+    # so we have to check the faces
+    if len(cards) == 0:
+        cards_with_faces = [card for card in scryfall_db if card.get('card_faces') != None]
+        cards = [card for card in cards_with_faces if card['set'].lower() == set_abbr.lower() and card['card_faces'][0]['name'].lower() == name.lower()]
+    if len(cards) == 0:
+        print(cards)
+        print(name, set_abbr)
+    default = min(cards, key=lambda card: card['collector_number'])['collector_number']
     assert type(default) == str
     assert default != ''
     return default
 
 
-if not os.path.exists('collector_numbers.json'):
-    os.mknod('collector_numbers.json')
+scryfall_db = []
 
-collector_numbers = {}
+with open('shrunk.json', 'r') as db:
+    scryfall_db = json.load(db)
 
-if os.path.getsize('collector_numbers.json') > 0:
-    with open('collector_numbers.json', 'r') as collector_numbers_file:
-        collector_numbers = json.load(collector_numbers_file)
-try:
-    with open('printable.txt', 'r') as f:
-        with open('output.csv', 'w') as out:
-            out.write(f"Quantity|Name|Set|Collector Number|Variation|List|Foil|Promo Pack|Prerelease|Language\n")
-            for line in f:
-                pattern = r'^([0-9]+)x ([^(]+) \(([A-Z0-9]{3,4})(:([A-Za-z0-9]+))?\)( \*(f|list|pp|f-pp|f-pre|[A-Z]{2})\*)?$'
-                matches = re.match(pattern, line)
-                if matches == None:
-                    raise Exception(f"Input is in wrong format. Expected to match '{pattern}' but couldn't. Input was {line}")
+with open('printable.txt', 'r') as f:
+    with open('output.csv', 'w') as out:
+        out.write(f"Quantity|Name|Set|Collector Number|Variation|List|Foil|Promo Pack|Prerelease|Language|Scryfall ID\n")
+        for line in f:
+            pattern = r'^([0-9]+)x ([^(]+) \(([A-Z0-9]{3,4})(:([A-Za-z0-9]+))?\)( \*(f|list|pp|f-pp|f-pre|[A-Z]{2})\*)?$'
+            matches = re.match(pattern, line)
+            if matches == None:
+                raise Exception(f"Input is in wrong format. Expected to match '{pattern}' but couldn't. Input was {line}")
 
-                quantity = matches.group(1)
-                name = matches.group(2)
-                set_abbr = matches.group(3)
-                if set_abbr == '000':
-                    if name == 'Arbor Elf':
-                        set_abbr = 'pw21'
-                    elif name == 'Archfiend of Ifnir':
-                        set_abbr = 'pakh'
-                    elif name == 'Ember Swallower':
-                        set_abbr = 'pths'
-                    elif name == 'Mind Stone':
-                        set_abbr = 'pw21'
-                    else:
-                        raise Exception(f"Unhandled 000 set. {name}")
-                # tappedout and scryfall use different codes
-                if set_abbr == 'MYS1':
-                    set_abbr = 'MB1'
-                elif set_abbr == 'EO2':
-                    set_abbr = 'E02'
-                elif set_abbr == 'PFL':
-                    set_abbr = 'PD2'
-                # These appear to be just wrong in tappedout?
-                elif set_abbr == 'TSB':
-                    if name in ['Swamp', 'Aarakocra Sneak']:
-                        set_abbr = 'CLB'
-                # 3 is a wrapper to make the varaition optional
-                variation = matches.group(5)
-                collector_number = None
-                # Collector numbers can have non-numbers in them
-                # this might not work :shrug:
-                if variation:
-                    if variation.isnumeric():
-                        collector_number = variation
-                        variation = None
-                    else:
-                        print(f"WARNING: Variation isn't numeric {name} {variation}")
+            quantity = matches.group(1)
+            name = matches.group(2)
+            set_abbr = matches.group(3)
+            if set_abbr == '000':
+                if name == 'Arbor Elf':
+                    set_abbr = 'pw21'
+                elif name == 'Archfiend of Ifnir':
+                    set_abbr = 'pakh'
+                elif name == 'Ember Swallower':
+                    set_abbr = 'pths'
+                elif name == 'Mind Stone':
+                    set_abbr = 'pw21'
                 else:
-                    key = f"{name}|{set_abbr}"
-                    if collector_numbers.get(key) == None:
-                        print(f"Looking up collector_number for {name} {set_abbr}")
-                        time.sleep(.1)
-                        collector_numbers[key] = get_default_collectors_number(name, set_abbr)
-                    collector_number = collector_numbers[key]
-                # 6 wraps around the *'s
-                foil_or_language = matches.group(7)
-                foil = False
-                language = "EN"
-                the_list = False
-                promo_pack = False
-                prerelease = False
-                if foil_or_language:
-                    if foil_or_language == 'f':
-                        foil = True
-                    elif foil_or_language == 'list':
-                        the_list = True
-                    elif foil_or_language == 'pp':
-                        promo_pack = True
-                    elif foil_or_language == 'f-pp':
-                        foil = True
-                        promo_pack = True
-                    elif foil_or_language == 'f-pre':
-                        foil = True
-                        prerelease = True
-                    else:
-                        language = foil_or_language
-
-                out.write(f"{quantity}|{name}|{set_abbr}|{collector_number}|{variation}|{the_list}|{foil}|{promo_pack}|{prerelease}|{language}\n")
-except BaseException as e:
-    with open('collector_numbers.json', 'w') as collector_numbers_file:
-        json.dump(collector_numbers, collector_numbers_file)
-    raise e
-
-with open('collector_numbers.json', 'w') as collector_numbers_file:
-    json.dump(collector_numbers, collector_numbers_file)
+                    raise Exception(f"Unhandled 000 set. {name}")
+            # tappedout and scryfall use different codes
+            if set_abbr == 'MYS1':
+                set_abbr = 'MB1'
+            elif set_abbr == 'EO2':
+                set_abbr = 'E02'
+            elif set_abbr == 'PFL':
+                set_abbr = 'PD2'
+            # These appear to be just wrong in tappedout?
+            elif set_abbr == 'TSB':
+                if name in ['Swamp', 'Aarakocra Sneak']:
+                    set_abbr = 'CLB'
+            # 3 is a wrapper to make the varaition optional
+            variation = matches.group(5)
+            collector_number = None
+            promo_pack = False
+            prerelease = False
+            # Collector numbers can have non-numbers in them
+            if variation:
+                if variation.isnumeric():
+                    collector_number = variation
+                    variation = None
+                elif variation == 'PromoPack':
+                    collector_number = get_default_collectors_number(name, set_abbr)
+                    promo_pack = True
+                    variation = None
+                else:
+                    raise Exception(f"Unhandled variation type. Variation was {variation}. Card was {name} {set_abbr}")
+            else:
+                collector_number = get_default_collectors_number(name, set_abbr)
+            # 6 wraps around the *'s
+            foil_or_language = matches.group(7)
+            foil = False
+            language = "EN"
+            the_list = False
+            if foil_or_language:
+                if foil_or_language == 'f':
+                    foil = True
+                elif foil_or_language == 'list':
+                    the_list = True
+                elif foil_or_language == 'pp':
+                    promo_pack = True
+                elif foil_or_language == 'f-pp':
+                    foil = True
+                    promo_pack = True
+                elif foil_or_language == 'f-pre':
+                    foil = True
+                    prerelease = True
+                else:
+                    language = foil_or_language
+            card = [card for card in scryfall_db if card['set'].lower() == set_abbr.lower() and card['collector_number'] == collector_number]
+            assert len(card) == 1
+            card = card[0]
+            scryfall_id = card['id']
+            out.write(f"{quantity}|{name}|{set_abbr}|{collector_number}|{variation}|{the_list}|{foil}|{promo_pack}|{prerelease}|{language}|{scryfall_id}\n")
 
 # Sanity checks
 import csv
@@ -131,7 +120,7 @@ with open('output.csv', 'r') as out:
     total_cards = 0
     for row in output_reader:
         total_cards += int(row["Quantity"])
-        assert(len(row.keys()) == 10)
+        assert(len(row.keys()) == 11)
 
 assert total_cards == 5668
 
---
+++ shrink_database.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys, json, time
+
+if len(sys.argv) > 2:
+    print("Expected exactly one argument. The path to the scryfall database")
+    sys.exit(1)
+
+elif len(sys.argv) < 2:
+    print("Expected exactly one argument. The path to the scryfall database")
+    sys.exit(2)
+
+scryfall_db_path = sys.argv[1]
+original_data = []
+shrunk_data = []
+
+
+start = time.time()
+with open(scryfall_db_path) as db:
+    # This is gonna take a long time and a lot of memory
+    # ~25 Seconds and ~5Gb
+    original_data = json.load(db)
+
+read_done = time.time()
+print(f'read done in: {read_done - start:.1f}')
+
+for card in original_data:
+    shrunk_card = {}
+    # A couple secret lair drops have the same card with
+    # different art on the front and back, (ex. Propaganda)
+    # these don't have an oracle_id on the outermost part
+    # of the dict, instead it's on each card face.
+    # So far, these are the only cards that work like this
+    # and both sides have the same oracle_id as the normal card
+    if not card.get('oracle_id'):
+        shrunk_card['oracle_id'] = card['card_faces'][0]['oracle_id']
+    else:
+        shrunk_card['oracle_id'] = card['oracle_id']
+
+    if card.get('card_faces'):
+        shrunk_card['card_faces'] = card['card_faces']
+
+    shrunk_card['name'] = card['name']
+    shrunk_card['collector_number'] = card['collector_number']
+    shrunk_card['set'] = card['set']
+    shrunk_card['id'] = card['id']
+
+    shrunk_data.append(shrunk_card)
+
+iterate_done = time.time()
+print(f'iterate done in: {iterate_done - read_done:.1f}')
+
+with open('shrunk.json', 'w') as out:
+    json.dump(shrunk_data, out)
+
+write_done = time.time()
+
+print(f'''write: {write_done - iterate_done:.1f}
+total: {write_done - start:.1f}''')
